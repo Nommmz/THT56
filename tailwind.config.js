1ecb8958d09f2c3bcfc52f9ade5d1d874d63fda5
Nommmz <anomwajaweningg@gmail.com>
2024-10-29 21:12:56 +0700

    index.html

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],  // Updated to point to src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
